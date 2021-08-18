b7f456fdc6e5cb4bc7ba3852629c6286e109d938
Laurence Zanotti <lau_zanotti@hotmail.com>
2021-08-17 23:08:59 -0300

    Primeiro app/formulário em react que fiz ever

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  // Fiz um formulário que coleta seu nome e exibe em um h1
+  // Primeiro app react ever, com certeza dá para melhorar
+  // 
+  // Fonte utilizadas:
+  // https://cs50.harvard.edu/web/2020/notes/6/#react
+  // https://pt-br.reactjs.org/docs/forms.html
+  
+  const [name, setName] = React.useState('World')
+
+  function updateName(e) {
+    setName(e.target.previousSibling.value ? e.target.previousSibling.value : 'World')
+    e.target.parentElement.reset()
+    e.preventDefault()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <h1>Hello {name}!</h1>
+      <label htmlFor="name">Your name:</label>
+      <input className="form-control mt-2" id="name" placeholder="Ex: John Doe"></input>
+      <input type="submit" className="btn btn-primary mt-2" onClick={updateName} value="Send"></input>
+    </form>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+        crossOrigin="anonymous"
+      />
   </React.StrictMode>,
   document.getElementById('root')
 );
